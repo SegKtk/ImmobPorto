724f1e12f5daa99b71e0b39fd6c6eafa7cb7f103
Segla KPATOUKPA <segla.kpatoukpa@imsp-uac.org>
2021-06-02 01:33:10 +0100

    3eme commit

--- affiche_annonce.php
+++ affiche_annonce.php
@@ -11,8 +11,14 @@
         $limite = 0;
 
 
+
          while(($ligne = $result->fetch(PDO::FETCH_ASSOC)) && ($limite < 9))
             {
+                $req1 = "select * from utilisateur,offres where utilisateur.id_user = ".$ligne['id_user'];
+                $result1 = $conn->prepare($req1);
+                $result1->execute();
+                $ligne1 = $result1->fetch(PDO::FETCH_ASSOC);
+
 
  ?>
 <div class="col mb-5">
@@ -23,9 +29,10 @@
         <div class="card-body p-4">
             <div class="text-center">
                                     <!-- Product name-->
-                <h5 class="fw-bolder">Fancy Product</h5>
+                <h5 class="fw-bolder">Par <?php echo $ligne1['nom']; ?></h5>
                                     <!-- Product price-->
                 <?php echo $ligne['prix']." FCFA"; ?>
+                <br>Contacter : <a href="http://<?php echo $ligne1['email']; ?>" class="link-primary"><?php echo $ligne1['email']; ?></a> 
             </div>
         </div>
                             <!-- Product actions-->
